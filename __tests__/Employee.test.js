1b3d6e989cdbebf24a109da87a7691b824a6fa52
Snehita Kolli <snehitakolli20@gmail.com>
2022-04-25 00:40:28 -0700

    all tests pass

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,10 +3,10 @@ const Employee = require('../lib/Employee')
 describe('Employee Class', () => {
     describe('Initialization', () => {
         it('Should create an object with an id and name', () => {
-            const employee = new Employee(123, 'Bob')
+            const employee = new Employee(123, 'Twiggy')
 
             expect(employee.id).toEqual(123);
-            expect(employee.name).toEqual('Bob');
+            expect(employee.name).toEqual('Twiggy');
         });
     });
 });
