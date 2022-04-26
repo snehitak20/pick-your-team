8b5b06ecceb18ab8ddb9f08e59b55490b93f11f0
Snehita Kolli <snehitakolli20@gmail.com>
2022-04-25 21:11:10 -0700

    employee test fixed and added to

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,12 +1,38 @@
 const Employee = require('../lib/Employee')
 
-describe('Employee Class', () => {
-    describe('Initialization', () => {
-        it('Should create an object with an id and name', () => {
-            const employee = new Employee(123, 'Twiggy')
+describe("Employee Class", () => {
+    describe("Employee object instantiation", () => {
+        it('Should create an object with an id, name and email', () => {
+            const employee = new Employee(123, 'Twiggy', 'twiggy123@email.com');
 
             expect(employee.id).toEqual(123);
             expect(employee.name).toEqual('Twiggy');
+            expect(employee.email).toEqual('twiggy123@email.com')
         });
     });
-});
+
+    describe("getId", () => {
+        it('Should return the id property of the object', () => {
+            const employee = new Employee(123, 'Twiggy', 'twiggy123@email.com');
+
+            expect(employee.getId()).toEqual(123);
+
+        });
+    });
+
+    describe("getName", () => {
+        it('Should return the name property of the object', () => {
+            const employee = new Employee(123, 'Twiggy', 'twiggy123@email.com');
+
+            expect(employee.getName()).toEqual("Twiggy");
+        });
+    });
+
+    describe("getEmail", () => {
+        it('Should return the email property of the object', () => {
+            const employee = new Employee(123, 'Twiggy', 'twiggy123@email.com');
+
+            expect(employee.getEmail()).toEqual("twiggy123@email.com");
+        });
+    });
+})
