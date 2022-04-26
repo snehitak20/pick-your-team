8d6597b72cb593292101908a16c84ed9ca3e9349
Snehita Kolli <snehitakolli20@gmail.com>
2022-04-25 21:28:10 -0700

    intern test fixed and added to

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,12 +1,50 @@
 const Intern = require('../lib/Intern')
 
-describe('Intern Class', () => {
-    describe('Initialization', () => {
-        it('Should create an object with an id and name', () => {
-            const intern = new Intern(123, 'Kate')
+describe("Intern Class", () => {
+    describe("Intern object instantiation", () => {
+        it('Should create an object with an id, name, email, and school', () => {
+            const intern = new Intern(123, 'Kate', 'kate123@email.com', 'TikTok Uni');
 
             expect(intern.id).toEqual(123);
             expect(intern.name).toEqual('Kate');
+            expect(intern.email).toEqual('kate123@email.com');
+            expect(intern.school).toEqual('TikTok Uni');
+        });
+    });
+
+    describe("getId", () => {
+        it('Should return the id property of the object', () => {
+            const intern = new Intern(123, 'Kate', 'kate123@email.com', 'TikTok Uni');
+
+            expect(intern.getId()).toEqual(123);
+
+        });
+    });
+
+    describe("getName", () => {
+        it('Should return the name property of the object', () => {
+            const intern = new Intern(123, 'Kate', 'kate123@email.com', 'TikTok Uni');
+
+            expect(intern.getName()).toEqual("Kate");
+
+        });
+    });
+
+    describe("getEmail", () => {
+        it('Should return the email property of the object', () => {
+            const intern = new Intern(123, 'Kate', 'kate123@email.com', 'TikTok Uni');
+
+            expect(intern.getEmail()).toEqual("kate123@email.com");
+
+        });
+    });
+
+    describe("getSchool", () => {
+        it('Should return the school property of the object', () => {
+            const intern = new Intern(123, 'Kate', 'kate123@email.com', 'TikTok Uni');
+
+            expect(intern.getSchool()).toEqual("TikTok Uni");
+
         });
     });
 });
